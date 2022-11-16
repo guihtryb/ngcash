87913e83be10be0746577ff956110e1175e8294f
Guilherme Viana <guilhermehviana01@gmail.com>
2022-11-16 20:06:07 -0300

    feat(frontend): Login page structure done

--- app/frontend/transfers-app/src/pages/Login.tsx
+++ app/frontend/transfers-app/src/pages/Login.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <main>
+      <h1>Login</h1>
+      <form onSubmit={(e) => { e.preventDefault(); }}>
+        <label htmlFor="input-login-email">
+          <input
+            type="email"
+            name="input-login-email"
+            id="input-login-email"
+            data-testid="input-login-email"
+          />
+        </label>
+        <label htmlFor="input-login-password">
+          <input
+            type="password"
+            name="input-login-password"
+            id="input-login-password"
+            data-testid="input-login-password"
+          />
+          <button type="submit">
+            Entrar
+          </button>
+        </label>
+      </form>
+      <p>
+        Ainda não possui uma conta?
+        {' '}
+        <Link to="/register">Registre-se</Link>
+      </p>
+    </main>
   );
 }
