e6f877a452740f088db3cb9841c70070da879a69
Guilherme Viana <guilhermehviana01@gmail.com>
2022-11-16 19:27:21 -0300

    chore(frontend): add initial setup

---
+++ app/frontend/transfers-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
